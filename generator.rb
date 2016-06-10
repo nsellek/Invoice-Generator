f3e643eec586ca9ab2f9ec09fb4782da0b2ce05e
nsellek <nick@seansellek.com>
2016-06-10 13:59:59 -0400

    added ablility to add hours to csv file

--- generator.rb
+++ generator.rb
@@ -1,2 +1,11 @@
 require 'csv'
 require './invoice.rb'
+
+# p ARGV
+
+case ARGV[0]
+when 'add_time'
+  InvoiceGenerator.add_time
+when 'send_invoice'
+  
+end
--- invoice.rb
+++ invoice.rb
@@ -1,6 +1,9 @@
 class InvoiceGenerator
   
   def self.add_time
-    
+    # p ARGV
+    CSV.open("./hours.csv", "a") do |csv|
+      csv << ARGV[1..3]
+    end
   end
 end
